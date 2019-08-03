3cc2e4ac5487600a5f3c5a1e606b85d9f1d04c28
Ian <i.r.butterworth@gmail.com>
2019-08-03 03:40:30 -0400

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using StressTest
-using Test
+using Test, Random
 
 @testset "dream(seconds) duration within 1% of target duration" begin
-    rng = Random.seed(1234)
-    for duration in (rand(rng,5) .* 2)
+    rng = Random.seed!(1234)
+    for duration in (rand(5) .* 2)
         e = @elapsed dream(duration)
         @test isapprox(e,duration,rtol=0.01)
     end
