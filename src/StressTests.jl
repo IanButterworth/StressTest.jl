0131117c71b00f37d30a10fdd15b3b0a6d2be268
Ian <i.r.butterworth@gmail.com>
2019-08-03 03:08:37 -0400

    Files generated by PkgTemplates

---
+++ src/StressTests.jl
@@ -0,0 +1,5 @@
+module StressTests
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using StressTests
+using Test
+
+@testset "StressTests.jl" begin
+    # Write your own tests here.
+end
