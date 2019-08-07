70fabc15cf3714701b2b03a5af9503fb9a8543e4
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-07 14:06:21 +0000

    build based on c11bf4d

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#StressTest.jl-1","page":"Home","title":"StressTest.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [StressTest]","category":"page"},{"location":"#StressTest.dream-Tuple{Real}","page":"Home","title":"StressTest.dream","text":"dream(seconds)\n\nLike Base.sleep() except maxes out the thread for a specified number of seconds. The minimum dream time is 1 millisecond or input of 0.001.\n\n\n\n\n\n","category":"method"}]
+}
