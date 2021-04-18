01d1f5234dafa0542454b9123207842100024dfb
Ian Butterworth <i.r.butterworth@gmail.com>
2021-04-18 01:19:40 -0400

    dream >  busywait, various updates (#3)
    * dream >  busywait, various updates
    
    * Delete .cirrus.yml
    
    * better busywait, add check_parallel_threading
    
    * Update Documentation.yml

--- docs/make.jl
+++ docs/make.jl
@@ -6,12 +6,12 @@ makedocs(;
     pages=[
         "Home" => "index.md",
     ],
-    repo="https://github.com/ianshmean/StressTest.jl/blob/{commit}{path}#L{line}",
+    repo="https://github.com/IanButterworth/StressTest.jl/blob/{commit}{path}#L{line}",
     sitename="StressTest.jl",
     authors="Ian Butterworth <i.r.butterworth@gmail.com>",
     assets=String[],
 )
 
 deploydocs(;
-    repo="github.com/ianshmean/StressTest.jl",
+    repo="github.com/IanButterworth/StressTest.jl",
 )
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
 using StressTest
 using Test, Random
 
-@testset "dream(seconds) duration within 1% of target duration" begin
+@testset "busywait(seconds) duration within 1% of target duration" begin
     rng = Random.seed!(1234)
     for duration in (rand(5) .* 2)
-        e = @elapsed dream(duration)
+        e = @elapsed busywait(duration)
         @test isapprox(e,duration,rtol=0.01)
     end
 end
